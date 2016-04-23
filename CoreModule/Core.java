a4dbb0618c5dbb4f5d1863d7b3729c47a0c38d02
erdemtoraman <erdemtoraman93@gmail.com>
2016-04-23 13:04:02 +0300

    recursively recursive

--- CoreModule/Core.java
+++ CoreModule/Core.java
@@ -14,18 +14,25 @@ import java.util.*;
 public class Core {
     public static ArrayList<YapımEki> yapımEkleri;
     public static ArrayList<CekimEki> cekimEkleri;
+    public static ArrayList<CekimEki> cekimEkleriFake = new ArrayList<>();
+    public static ArrayList<YapımEki> yapımEkleriFake = new ArrayList<>();
     public static HashSet<Word> turkish;
+    public static ArrayList<String> allEks = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         yapımEkleri = new ArrayList<>();
         cekimEkleri = new ArrayList<>();
         turkish = new HashSet<>();
+
         readSuffixes("yapımekleri.txt", "çekimekleri.txt", "TurkishRoots.txt");
+        cekimEkleriFake.addAll(cekimEkleri);
+        yapımEkleriFake.addAll(yapımEkleri);
         Scanner scan = new Scanner(System.in);
         String input = "";
-        while(!input.equals("q")) {
+        while (!input.equals("q")) {
             input = scan.next();
-            testWords(input);
+            ArrayList<String> answer = allProducable(input);
+            System.out.println(answer);
         }
 
     }
@@ -33,8 +40,9 @@ public class Core {
     private static void testWords(String input) {
 
         for (int i = 1; i < input.length(); i++) {
+            ArrayList<WordDetail> wds = new ArrayList<>();
             if (turkish.contains(new Word(input.substring(0, i), ""))) {
-                ArrayList<WordDetail> w1 = isEklerProducable(input.substring(i), new Word(input.substring(0, i), ""),input.substring(i));
+                WordDetail w1 = isEklerProducable(input.substring(i), new Word(input.substring(0, i), ""), input.substring(i));
                 System.out.println();
             }
         }
@@ -65,54 +73,81 @@ public class Core {
         return result;
     }
 
-    public static ArrayList<WordDetail> isEklerProducable(String ekler, Word w, String initial) {
-        ArrayList<WordDetail> resultWordDetails = new ArrayList<WordDetail>();
-        Boolean found = false;
+    public static WordDetail isEklerProducable(String ekler, Word w, String initial) {
+
         if (cekimEkleri.contains(new CekimEki(ekler, ""))) {
             ArrayList<Ek> eks = new ArrayList<>();
             eks.add(new CekimEki(ekler, ""));
 
-            resultWordDetails.add(new WordDetail(w.getContent(), eks));
-            found = true;
+            return new WordDetail(w.getContent(), eks);
+
         }
         if (yapımEkleri.contains(new YapımEki(ekler, "", ""))) {
             ArrayList<Ek> eks = new ArrayList<>();
             eks.add(new YapımEki(ekler, "", ""));
 
-            resultWordDetails.add(new WordDetail(w.getContent(), eks));
-            found = true;
+            return (new WordDetail(w.getContent(), eks));
+
         }
-        if (found) return resultWordDetails;
         for (int i = 1; i < ekler.length(); i++) {
             String candidate1 = ekler.substring(0, i);
             String candidate2 = ekler.substring(i);
-            ArrayList<WordDetail> w1 = isEklerProducable(candidate1, w, initial);
-            ArrayList<WordDetail> w2 = isEklerProducable(candidate2, w,initial);
-            if (w1 != null && w2 != null) {
-                for (WordDetail wd:
-                        w1) {
-                    for (WordDetail wdIn:
-                            w2) {
-
-                       WordDetail listElement = new WordDetail(w.getContent(), null);
-                        listElement.setEkler(wd.getEkler());
-                        listElement.getEkler().addAll(wdIn.getEkler());
-                        String tot = "";
-                        for (Ek e:listElement.getEkler()) {
-                            tot +=e.getEk();
-                        }
-                        if(tot.length() <= initial.length())
-                        resultWordDetails.add(listElement);
-                    }
+            WordDetail w1 = isEklerProducable(candidate1, w, initial);
+            if (w1 != null) {
+                WordDetail w2 = isEklerProducable(candidate2, w, initial);
+                if (w1 != null && w2 != null) {
+                    WordDetail listElement = new WordDetail(w.getContent(), null);
+                    listElement.setEkler(w1.getEkler());
+                    listElement.getEkler().addAll(w2.getEkler());
+                    return listElement;
                 }
-                return resultWordDetails;
             }
+
         }
 
         return null;
     }
 
+    public static ArrayList<String> allProducable(String sequence) {
+        ArrayList<String> allPoss = new ArrayList<>();
+        String res = "";
+        if (allEks.contains(sequence)) allPoss.add(sequence);
+        for (int i = 1; i < sequence.length(); i++) {
+            String left = sequence.substring(0, i);
+            String right = sequence.substring(i);
+            if (allEks.contains(left) && allEks.contains(right)) {
+                allPoss.add(left + "-" + right);
+            } else {
+                ArrayList<String> leftPoss = allProducable(left);
+                ArrayList<String> rightPoss = allProducable(right);
+                if (leftPoss != null && rightPoss != null) {
+                    for (String leftPos : leftPoss) {
+                        for (String rightPos : rightPoss) {
+                            allPoss.add(leftPos + "+" + rightPos);
+                        }
 
+                    }
+                }
+            }
+
+        }
+        if (allPoss.size() > 0) return allPoss;
+        return null;
+    }
+
+    public static String producable(String sequence) {
+        if (allEks.contains(sequence)) {
+            return sequence;
+        }
+        for (int i = 1; i < sequence.length(); i++) {
+            String temp1 = producable(sequence.substring(0, i));
+            String temp2 = producable(sequence.substring(i));
+            if (temp1 != null && temp2 != null) {
+                return temp1 + "-" + temp2;
+            }
+        }
+        return null;
+    }
 
     private static void readSuffixes(String filepath1, String filepath2, String filepath3) throws IOException {
         BufferedReader read = new BufferedReader(new FileReader(new File(filepath1)));
@@ -127,6 +162,7 @@ public class Core {
                 ek.setTo(actions[1]);
                 ek.setEk(ekler[i]);
                 yapımEkleri.add(ek);
+                allEks.add(ek.getEk());
             }
         }
         read.close();
@@ -137,7 +173,8 @@ public class Core {
             String[] types = temp2[1].trim().split(" ");
             for (String type :
                     types) {
-                  cekimEkleri.add(new CekimEki(ek, type));
+                cekimEkleri.add(new CekimEki(ek, type));
+                allEks.add(ek);
             }
         }
         read.close();
